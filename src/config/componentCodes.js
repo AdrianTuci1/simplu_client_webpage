d8570949afed1d864ea19be019930df691f86f68
Tucian <adrian.tucicovenco@gmail.com>
2025-05-24 03:57:12 +0300

    optimised homepage components

---
+++ src/config/componentCodes.js
@@ -0,0 +1,55 @@
+import {
+  HeroVariant1,
+  HeroVariant2,
+  HeroVariant3
+} from '../components/Hero';
+import {
+  FeaturesVariant1,
+  FeaturesVariant2,
+  FeaturesVariant3
+} from '../components/Features';
+import {
+  DescriptionVariant1,
+  DescriptionVariant2
+} from '../components/Description';
+import {
+  TestimonialsVariant1,
+  TestimonialsVariant2
+} from '../components/Testimonials';
+import {
+  GalleryVariant1,
+  GalleryVariant2
+} from '../components/Gallery';
+import {
+  ContactVariant1,
+  ContactVariant2
+} from '../components/Contact';
+
+// Mapare coduri componente
+export const COMPONENT_CODES = {
+  // Hero variants
+  11: HeroVariant1,
+  12: HeroVariant2,
+  13: HeroVariant3,
+
+  // Features variants
+  21: FeaturesVariant1,
+  22: FeaturesVariant2,
+  23: FeaturesVariant3,
+
+  // Description variants
+  61: DescriptionVariant1,
+  62: DescriptionVariant2,
+
+  // Testimonials variants
+  31: TestimonialsVariant1,
+  32: TestimonialsVariant2,
+
+  // Gallery variants
+  41: GalleryVariant1,
+  42: GalleryVariant2,
+
+  // Contact variants
+  51: ContactVariant1,
+  52: ContactVariant2
+}; 
